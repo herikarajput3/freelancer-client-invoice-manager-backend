9d83f10083074f5b67e35c728204948f195724e7
Herika <herikarajput3@gmail.com>
2026-08-14 16:51:54 +0530

    feat: add standardized API response utility

--- src/core/responses/sendResponse.js
+++ src/core/responses/sendResponse.js
@@ -1,21 +1,41 @@
-function sendResponse(
+const sendResponse = (
     res,
     {
         statusCode,
         success,
         message,
         data,
-        meta
+        pagination,
+        error,
+        errors,
     }
-) {
+) => {
+    if (statusCode === 204) {
+        return res.status(statusCode).send();
+    }
+
     const response = {
         success,
         message,
-        data,
-        meta,
     };
 
-    res.status(statusCode).json(response);
-}
+    if (data !== undefined) {
+        response.data = data;
+    }
+
+    if (pagination !== undefined) {
+        response.pagination = pagination;
+    }
+
+    if (error !== undefined) {
+        response.error = error;
+    }
+
+    if (errors !== undefined) {
+        response.errors = errors;
+    }
+
+    return res.status(statusCode).json(response);
+};
 
 export default sendResponse;
